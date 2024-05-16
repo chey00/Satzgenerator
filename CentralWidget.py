d2b149bc32e692e6530663a4040a05965e0df758
Christian Hey <c.hey@sbs-herzogenaurach.de>
2024-05-16 12:55:20 +0200

    Simple soulution

--- CentralWidget.py
+++ CentralWidget.py
@@ -11,59 +11,63 @@ class CentralWidget(QWidget):
         label_1 = QLabel("Subjekt")
         layout.addWidget(label_1, 1, 1, 1, 4)
 
-        push_button_1_1 = QPushButton("Hansi")
-        push_button_1_1.pressed.connect(self.handle_first_row)
-        layout.addWidget(push_button_1_1, 2, 1)
+        self.push_button_1_1 = QPushButton("Hansi")
+        self.push_button_1_1.pressed.connect(self.handle_1)
+        layout.addWidget(self.push_button_1_1, 2, 1)
 
-        push_button_1_2 = QPushButton("Das Auto")
-        push_button_1_2.pressed.connect(self.handle_first_row)
-        layout.addWidget(push_button_1_2, 2, 2)
+        self.push_button_1_2 = QPushButton("Das Auto")
+        self.push_button_1_2.pressed.connect(self.handle_1)
+        layout.addWidget(self.push_button_1_2, 2, 2)
 
-        push_button_1_3 = QPushButton("Herr Hey")
-        push_button_1_3.pressed.connect(self.handle_first_row)
-        layout.addWidget(push_button_1_3, 2, 3)
+        self.push_button_1_3 = QPushButton("Herr Hey")
+        self.push_button_1_3.pressed.connect(self.handle_1)
+        layout.addWidget(self.push_button_1_3, 2, 3)
 
-        push_button_1_4 = QPushButton("Deine Mutter")
-        push_button_1_4.pressed.connect(self.handle_first_row)
-        layout.addWidget(push_button_1_4, 2, 4)
+        self.push_button_1_4 = QPushButton("Deine Mutter")
+        self.push_button_1_4.pressed.connect(self.handle_1)
+        layout.addWidget(self.push_button_1_4, 2, 4)
 
         label_2 = QLabel("Prädikat")
         layout.addWidget(label_2, 3, 1, 1, 4)
 
-        push_button_2_1 = QPushButton("trägt")
-        push_button_2_1.pressed.connect(self.handle_first_row)
-        layout.addWidget(push_button_2_1, 4, 1)
+        self.push_button_2_1 = QPushButton("trägt")
+        self.push_button_2_1.pressed.connect(self.handle_2)
+        layout.addWidget(self.push_button_2_1, 4, 1)
 
-        push_button_2_2 = QPushButton("isst")
-        push_button_2_2.pressed.connect(self.handle_first_row)
-        layout.addWidget(push_button_2_2, 4, 2)
+        self.push_button_2_2 = QPushButton("isst")
+        self.push_button_2_2.pressed.connect(self.handle_2)
+        layout.addWidget(self.push_button_2_2, 4, 2)
 
-        push_button_2_3 = QPushButton("sieht")
-        push_button_2_3.pressed.connect(self.handle_first_row)
-        layout.addWidget(push_button_2_3, 4, 3)
+        self.push_button_2_3 = QPushButton("sieht")
+        self.push_button_2_3.pressed.connect(self.handle_2)
+        layout.addWidget(self.push_button_2_3, 4, 3)
 
-        push_button_2_4 = QPushButton("trinkt")
-        push_button_2_4.pressed.connect(self.handle_first_row)
-        layout.addWidget(push_button_2_4, 4, 4)
+        self.push_button_2_4 = QPushButton("trinkt")
+        self.push_button_2_4.pressed.connect(self.handle_2)
+        layout.addWidget(self.push_button_2_4, 4, 4)
+
+        self.disable_buttons_2(True)
 
         label_3 = QLabel("Objekt")
         layout.addWidget(label_3, 5, 1, 1, 4)
 
-        push_button_3_1 = QPushButton("einen Porsche.")
-        push_button_3_1.pressed.connect(self.handle_last_row)
-        layout.addWidget(push_button_3_1, 6, 1)
+        self.push_button_3_1 = QPushButton("einen Porsche.")
+        self.push_button_3_1.pressed.connect(self.handle_3)
+        layout.addWidget(self.push_button_3_1, 6, 1)
 
-        push_button_3_2 = QPushButton("Pommes.")
-        push_button_3_2.pressed.connect(self.handle_last_row)
-        layout.addWidget(push_button_3_2, 6, 2)
+        self.push_button_3_2 = QPushButton("Pommes.")
+        self.push_button_3_2.pressed.connect(self.handle_3)
+        layout.addWidget(self.push_button_3_2, 6, 2)
 
-        push_button_3_3 = QPushButton("Cola.")
-        push_button_3_3.pressed.connect(self.handle_last_row)
-        layout.addWidget(push_button_3_3, 6, 3)
+        self.push_button_3_3 = QPushButton("Cola.")
+        self.push_button_3_3.pressed.connect(self.handle_3)
+        layout.addWidget(self.push_button_3_3, 6, 3)
 
-        push_button_3_4 = QPushButton("die Sterne.")
-        push_button_3_4.pressed.connect(self.handle_last_row)
-        layout.addWidget(push_button_3_4, 6, 4)
+        self.push_button_3_4 = QPushButton("die Sterne.")
+        self.push_button_3_4.pressed.connect(self.handle_3)
+        layout.addWidget(self.push_button_3_4, 6, 4)
+
+        self.disable_buttons_3(True)
 
         label_4 = QLabel("Textausgabe:")
         layout.addWidget(label_4, 7, 1, 1, 4)
@@ -72,19 +76,54 @@ class CentralWidget(QWidget):
         layout.addWidget(self.text_browser, 8, 1, 1, 4)
 
         self.setLayout(layout)
+        
+    def disable_buttons_1(self, disable):
+        self.push_button_1_1.setDisabled(disable)
+        self.push_button_1_2.setDisabled(disable)
+        self.push_button_1_3.setDisabled(disable)
+        self.push_button_1_4.setDisabled(disable)
+
+    def disable_buttons_2(self, disable):
+        self.push_button_2_1.setDisabled(disable)
+        self.push_button_2_2.setDisabled(disable)
+        self.push_button_2_3.setDisabled(disable)
+        self.push_button_2_4.setDisabled(disable)
+
+    def disable_buttons_3(self, disable):
+        self.push_button_3_1.setDisabled(disable)
+        self.push_button_3_2.setDisabled(disable)
+        self.push_button_3_3.setDisabled(disable)
+        self.push_button_3_4.setDisabled(disable)
 
     @pyqtSlot()
-    def handle_first_row(self):
+    def handle_1(self):
         text = self.text_browser.toPlainText()
         text += self.sender().text() + " "
 
         self.text_browser.setText(text)
 
+        self.disable_buttons_1(True)
+        self.disable_buttons_2(False)
+
     @pyqtSlot()
-    def handle_last_row(self):
+    def handle_2(self):
+        text = self.text_browser.toPlainText()
+        text += self.sender().text() + " "
+
+        self.text_browser.setText(text)
+
+        self.disable_buttons_2(True)
+        self.disable_buttons_3(False)
+
+
+    @pyqtSlot()
+    def handle_3(self):
         text = self.text_browser.toPlainText()
         text += self.sender().text() + "\n"
 
         self.text_browser.setText(text)
+
+        self.disable_buttons_3(True)
+        self.disable_buttons_1(False)
 
 
